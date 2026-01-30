47cc8e37d3daa9542321d6a0a65aa7006003d16d
Toos <151813251+xymn2023@users.noreply.github.com>
2026-01-30 14:17:47 +0800

    update zb.json

---
+++ zb.js
@@ -0,0 +1,42 @@
+[
+  {
+    "name": "TV直播",
+    "url": "https://gh-proxy.org/https://gh.kejilion.pro/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg1"
+  },
+  {
+    "name": "冰茶TV直播",
+    "url": "https://bc.188766.xyz/?ip=&mima=bingcha1130&huikan=1&json=true"
+  },
+  {
+    "name": "宫殿TV直播",
+    "url": "https://gongdian.top/tv/iptv"
+  },
+  {
+    "name": "猫影TV直播",
+    "url": "https://iptv.catvod.com/tv.m3u"
+  },
+{
+    "name": "白雪TV直播",
+    "url": "http://bxtv.3a.ink/live.txt"
+  },
+  {
+    "name": "日后TV直播",
+    "url": "http://rihou.cc:555/gggg.nzk"
+  },
+{
+    "name": "易发TV直播",
+    "url": "https://gh-proxy.org/https://gh.kejilion.pro/https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt"
+  },
+{
+    "name": "潇雨萌萌TV直播",
+    "url": "https://ds65.tv1288.xyz"
+  },
+{
+    "name": "JackTV直播{翻}",
+    "url": "https://php.946985.filegear-sg.me/jackTV.m3u"
+  },
+  {
+    "name": "四季TV直播{台湾ip翻}",
+    "url": "http://ezc.xtvantsc.xyz/litv.php"
+  }
+]
